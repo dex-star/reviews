7b20fe50d3b46d95e73485c13d4260503e5bf792
Germain <rocg970209@icloud.com>
2019-08-10 19:58:57 -0500

    update
    conexion

--- conexion.php
+++ conexion.php
@@ -1,7 +1,11 @@
 <?php
-	$mysqli = new mysqli('dexreviews.com.mx', 'germain', 'K1ssmean1mal!', 'reviews');
-    //comprobar conexion
-    if($mysqli->connect_errno):
-		echo "Error al conectarse con MySQL debido al error" .$mysqli->connect_error;
-    endif;
+	$conexion= new mysqli("dexreviews.com.mx", "germain", "K1ssmean1mal!", "reviews");
+	//Comprobar conexion
+	if(mysqli_connect_errno())
+	{
+		printf("Fallo la conexion");
+	}
+	else {
+		//printf("Estas conectado");
+	}
 ?>
