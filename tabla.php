f20abc558481fed8ee185958f4cc99b0b0c7704e
Germain <rocg970209@icloud.com>
2019-08-10 19:42:12 -0500

    Update tabla.php
    Prueba de conexion

--- tabla.php
+++ tabla.php
@@ -1,37 +1,37 @@
-    <!DOCTYPE html>
-    <html lang="es">
+<!DOCTYPE html>
+<html lang="es">
 
-    <head>
-        <meta charset="UTF-8">
-        <title>Document</title>
-    </head>
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
 
-    <body>
-        <div id="contenido">
-            <table>
-                <thead>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                    <th><a href="nuevo.php"><button type="button" class="btn btn-info">Nuevo</button></a></th>
-                </thead>
-                
-        <?php
-        include "php/conexion.php";
-        $consulta="select * from USUARIOS";
-        $resultado=mysql_query($consulta);
-        while($filas=mysql_fetch_assoc($resultado)){
-         echo "<tr>";   
-            echo "<td>"; echo $filas['U_ID'];echo "</td>";
-            echo "<td>"; echo $filas['U_NOMBRE'];echo "</td>";
-            echo "<td>"; echo $filas['U_MAIL'];echo "</td>";
-            echo "<td>  <a href='modificar.php?U_ID=".$filas['U_ID']."'> <button type='button' class='btn btn-success'>Modificar</button> </a> </td>";
-            echo "<td> <a href='eliminar.php?U_ID=".$filas['U_ID']."''><button type='button' class='btn btn-danger'>Eliminar</button></a> </td>";
-        }
-        ?>
-        
-        </table>
-    </div>
+<body>
+  <div id="contenido">
+    <table>
+      <thead>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Email</th>
+        <th><a href="nuevo.php"><button type="button" class="btn btn-info">Nuevo</button></a></th>
+      </thead>
+
+      <?php
+      include "php/conexion.php";
+      $consulta="SELECT * FROM USUARIOS";
+      $resultado=$conexion->query($consulta) or die (mysqli_error($conexion));
+      while($filas=$resultado->fetch_assoc()){
+        echo "<tr>";
+        echo "<td>"; echo $filas['U_ID'];echo "</td>";
+        echo "<td>"; echo $filas['U_NOMBRE'];echo "</td>";
+        echo "<td>"; echo $filas['U_MAIL'];echo "</td>";
+        echo "<td>  <a href='modificar.php?U_ID=".$filas['U_ID']."'> <button type='button' class='btn btn-success'>Modificar</button> </a> </td>";
+        echo "<td> <a href='eliminar.php?U_ID=".$filas['U_ID']."''><button type='button' class='btn btn-danger'>Eliminar</button></a> </td>";
+        echo "</tr>";
+      }
+      ?>
+
+    </table>
+  </div>
 </body>
-
 </html>
