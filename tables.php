fff39f67611a0a0a689e225f9cb36e01b3db6039
Germain <rocg970209@icloud.com>
2019-08-10 23:49:44 -0500

    tablas
    probando

--- tables.php
+++ tables.php
@@ -118,18 +118,32 @@
                       <thead>
                         <tr>
                           <th>#</th>
-                          <th>First Name</th>
-                          <th>Last Name</th>
-                          <th>Username</th>
+                          <th>Nombre</th>
+                          <th>Email</th>
+                          <th><a href="nuevo.php"><button type="button" class="btn btn-info">Nuevo</button></a></th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <th scope="row">1</th>
-                          <td>Mark</td>
-                          <td>Otto</td>
-                          <td>@mdo</td>
-                        </tr>
+
+                        <?php
+                        include "php/conexion.php";
+                        $consulta="SELECT * FROM USUARIOS";
+                        $resultado=$conexion->query($consulta) or die (mysqli_error($conexion));
+                        while($filas=$resultado->fetch_assoc()){
+                          echo "<tr>";
+                          echo "<td scope="row">"; echo $filas['U_ID'];echo "</td>";
+                          echo "<td>"; echo $filas['U_NOMBRE'];echo "</td>";
+                          echo "<td>"; echo $filas['U_MAIL'];echo "</td>";
+                          echo "<td>  <a href='modificar.php?U_ID=".$filas['U_ID']."'> <button type='button' class='btn btn-success'>Modificar</button> </a> </td>";
+                          echo "<td> <a href='eliminar.php?U_ID=".$filas['U_ID']."''><button type='button' class='btn btn-danger'>Eliminar</button></a> </td>";
+                          echo "</tr>";
+                        }
+                        ?>
+
+                  
+
+
+
                       </tbody>
                     </table>
                   </div>
